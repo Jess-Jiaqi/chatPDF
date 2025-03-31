76e6d3972f5f1df6bfe99c6402719f10824b04e8
Jess-Jiaqi <li.jiaqi5@northeastern.edu>
2025-03-31 00:17:59 -0700

    config DB

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,21 @@
+import { integer, pgEnum, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const userSystemEnum = pgEnum("user_system_enum", ["system", "user"])
+
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  pdfName: text("pdfName").notNull(),
+  pdfUrl: text("pdfUrl").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  userId: varchar("user_id", { length: 256 }).notNull(),
+  fileKey: text("file_key").notNull(),
+})
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatId: integer("chat_id").references(() => chats.id).notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  role: userSystemEnum("role").notNull(),
+})
