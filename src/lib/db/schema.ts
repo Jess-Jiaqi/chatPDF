652819a9fd391b5315c0665cc81a18770399612b
Jess-Jiaqi <li.jiaqi5@northeastern.edu>
2025-04-05 23:08:39 -0700

    chat sidebar & pdf viewer

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,6 +1,14 @@
-import { integer, pgEnum, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const userSystemEnum = pgEnum("user_system_enum", ["system", "user"])
+export const userSystemEnum = pgEnum("user_system_enum", ["system", "user"]);
 
 export const chats = pgTable("chats", {
   id: serial("id").primaryKey(),
@@ -10,12 +18,16 @@ export const chats = pgTable("chats", {
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
   userId: varchar("user_id", { length: 256 }).notNull(),
   fileKey: text("file_key").notNull(),
-})
+});
+
+export type DrizzleChat = typeof chats.$inferSelect;
 
 export const messages = pgTable("messages", {
   id: serial("id").primaryKey(),
-  chatId: integer("chat_id").references(() => chats.id).notNull(),
+  chatId: integer("chat_id")
+    .references(() => chats.id)
+    .notNull(),
   content: text("content").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   role: userSystemEnum("role").notNull(),
-})
+});
