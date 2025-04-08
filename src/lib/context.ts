a9b20bac65e56e28356020eea3b34494abd2e391
Jess-Jiaqi <li.jiaqi5@northeastern.edu>
2025-04-08 16:39:25 -0700

    send Pinecone vectors to OpenAI & change embedding model from hf to openai

---
+++ src/lib/context.ts
@@ -0,0 +1,42 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { convertToAscii } from "./utils";
+import { getEmbeddings } from "./embeddings";
+export async function getMatchesFromEmbeddings(
+  embeddings: number[],
+  file_key: string
+) {
+  const pinecone = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY!,
+  });
+  const index = await pinecone.Index("chatpdf");
+
+  try {
+    const namespace = convertToAscii(file_key);
+    const queryResult = await index.namespace(namespace).query({
+      topK: 5,
+      vector: embeddings,
+      includeMetadata: true,
+    });
+    return queryResult.matches || [];
+  } catch (error) {
+    console.log("error querying embeddings", error);
+    throw error;
+  }
+}
+
+export async function getContext(query: string, file_key: string) {
+  const queryEmbeddings = await getEmbeddings(query);
+  const matches = await getMatchesFromEmbeddings(queryEmbeddings, file_key);
+
+  const qualifyingDocs = matches.filter(
+    (match) => match.score && match.score > 0.5
+  );
+
+  type Metadata = {
+    text: string;
+    pageNumber: number;
+  };
+
+  let docs = qualifyingDocs.map((match) => (match.metadata as Metadata).text);
+  return docs.join("\n").substring(0, 3000);
+}
